33effee429f2d90ec39bc013775f53e2aca29583
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-20 15:16:30 -0300

    curso react 60%
    Como integrar CSS dentro da sua aplicação React
    
    Diferenças do class e className
    
    Propriedade "key" dos componentes
    
    Padrão de nomenclatura do CSS
    
    Tratando o conflito de nomes
    
    Ponto de entrada dos módulos
    
    Organização do projeto

--- Entendendo-como-a-biblioteca-funciona/ceep/src/App.js
+++ Entendendo-como-a-biblioteca-funciona/ceep/src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { FormularioCadastro } from './components/FormularioCadastro.jsx';
-import { ListaDeNotas } from './components/ListaDeNotas.jsx';
-
-export class App extends  Component {
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
   render() {
     return (
-      <section>
-       
-        <FormularioCadastro/>
-  
-        <ListaDeNotas/>
-  
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
     );
   }
 }
 
-
+export default App;
