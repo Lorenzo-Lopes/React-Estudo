be700d5088787f19721fa359ead50b31bceb61c3
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2024-03-30 22:30:47 -0300

    criado estrutura e direcionamento de paginas

--- app-costs/src/App.jsx
+++ app-costs/src/App.jsx
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route}  from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import NavBar from './components/NavBar';
+import Container from './components/layout/Container';
+
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <NavBar/>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' Component={Home}/>
+          <Route path='/company' Component={Company}/>
+          <Route path='/contact' Component={Contact}/>
+          <Route path='/newproject' Component={NewProject}/>
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
